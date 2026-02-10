4f1350a7705afdf43b37d2394dcb4c6d0da7e48e
Anubhav Tyagi <tyagiianii009@gmail.com>
2026-02-10 22:54:54 +0530

    Spring security

---
+++ src/main/java/com/anubhavtyagi28/springsecurity/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.anubhavtyagi28.springsecurity.repositories;
+
+import com.anubhavtyagi28.springsecurity.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
---
+++ src/main/java/com/anubhavtyagi28/springsecurity/services/UserService.java
@@ -0,0 +1,23 @@
+package com.anubhavtyagi28.springsecurity.services;
+
+import com.anubhavtyagi28.springsecurity.exceptions.ResourceNotFoundException;
+import com.anubhavtyagi28.springsecurity.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found with email: "+username));
+    }
+}
